c6b811e08eb04d3a989ae64506b5517810d5c861
Taha Hachana <tahahachana@live.com>
2012-03-11 12:47:25 +0100

    Rewrote stop words loading function.

--- Src/Keywords.fs
+++ Src/Keywords.fs
@@ -1,46 +1,46 @@
-﻿module Keywords
+﻿namespace SEOLib
 
 open System
 open System.IO
 open System.Net
 open System.Text.RegularExpressions
-open Helpers
+open StopWords
 open Types
+open Utilities
 
-/// Loads a stop words list for the given language.
-let loadStopWords =
-    function
-        | English -> readAllLines "englishStopWords.txt"
-        | French  -> readAllLines "frenchStopWords.txt"
+module Keywords =
 
-/// Removes stop words form a string.
-let rec removeStopWords str stopWords =
-    match stopWords with
-    | h :: t ->
-        let str' = Regex("\\b" + h + "\\b").Replace(str, "0")
-        removeStopWords str' t
-    | [] -> str
+    /// Loads a stop words list for the given language.
+    let loadStopWords =
+        function
+            | English -> Array.toList englishStopWords
+            | French  -> Array.toList frenchStopWords
 
-let oneKeywordRegex    = compileRegex "[^0-9\W]+"
-let twoKeywordsRegex   = compileRegex "[^0-9\W]+ [^0-9\W]+"
-let threeKeywordsRegex = compileRegex "[^0-9\W]+ [^0-9\W]+ [^0-9\W]+"
+    /// Removes stop words form a string.
+    let rec removeStopWords str stopWords =
+        match stopWords with
+        | h :: t ->
+            let regex = compileRegex <| "\\b" + h + "\\b" 
+            let str'  = regex.Replace(str, "0")
+            removeStopWords str' t
+        | [] -> str
 
-let keywordData (matchCollection : Match seq) str count length =
-    matchCollection
-    |> Seq.map (fun x -> x.Value)
-    |> Seq.distinct
-    |> Seq.map (fun x -> x, Regex("\\b" + x + "\\b").Matches(str).Count)
-    |> Seq.filter (fun (_, x) -> x > 1)
-    |> Seq.map (fun (x, y) -> x, y, float y / count * 100. * length)
-    |> Seq.map roundThird
-    |> Seq.sortBy (fun (_, x, _) -> x)
-    |> Seq.toArray
-    |> Array.rev
+    let keywordData (matchCollection : Match seq) str count length =
+        matchCollection
+        |> Seq.map (fun x -> x.Value)
+        |> Seq.distinct
+        |> Seq.map (fun x -> x, Regex("\\b" + x + "\\b").Matches(str).Count)
+        |> Seq.filter (fun (_, x) -> x > 1)
+        |> Seq.map (fun (x, y) -> x, y, computeDensity y count length)
+        |> Seq.map roundThird
+        |> Seq.sortBy (fun (_, x, _) -> x)
+        |> Seq.toArray
+        |> Array.rev
 
-let keywordData' regex str count length =
-    let matchCollection = regexMatches regex str
-    keywordData matchCollection str count length
+    let keywordData' regex str count length =
+        let matchCollection = regexMatches regex str
+        keywordData matchCollection str count length
 
-let twoKeywordsData str count length = keywordData' twoKeywordsRegex str count length
+    let twoKeywordsData str count length = keywordData' twoKeywordsRegex str count length
 
-let threeKeywordsData str count length = keywordData' threeKeywordsRegex str count length
+    let threeKeywordsData str count length = keywordData' threeKeywordsRegex str count length
