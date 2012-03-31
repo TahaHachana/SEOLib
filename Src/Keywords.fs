28f398879e661d0921b34d282307ecbd8212e620
Taha Hachana <tahahachana@live.com>
2012-03-31 13:02:32 +0100

    Added a higher order function to make analyzing a Web page's keywords a one liner function call.

--- Src/Keywords.fs
+++ Src/Keywords.fs
@@ -4,6 +4,7 @@ open System
 open System.IO
 open System.Net
 open System.Text.RegularExpressions
+open HTML
 open StopWords
 open Types
 open Utilities
@@ -11,21 +12,21 @@ open Utilities
 module Keywords =
 
     /// Loads a stop words list for the given language.
-    let loadStopWords =
+    let private loadStopWords =
         function
             | English -> Array.toList englishStopWords
             | French  -> Array.toList frenchStopWords
 
     /// Removes stop words form a string.
-    let rec removeStopWords str stopWords =
+    let rec private removeStopWords stopWords str =
         match stopWords with
         | h :: t ->
             let regex = compileRegex <| "\\b" + h + "\\b" 
             let str'  = regex.Replace(str, "0")
-            removeStopWords str' t
+            removeStopWords t str'
         | [] -> str
 
-    let keywordData (matchCollection : Match seq) str count length =
+    let private keywordData (matchCollection : Match seq) str count length =
         matchCollection
         |> Seq.map (fun x -> x.Value)
         |> Seq.distinct
@@ -37,10 +38,22 @@ module Keywords =
         |> Seq.toArray
         |> Array.rev
 
-    let keywordData' regex str count length =
+    let private keywordData' regex str count length =
         let matchCollection = regexMatches regex str
         keywordData matchCollection str count length
 
-    let twoKeywordsData str count length = keywordData' twoKeywordsRegex str count length
+    let private twoKeywordsData str count = keywordData' twoKeywordsRegex str count 2.
 
-    let threeKeywordsData str count length = keywordData' threeKeywordsRegex str count length
+    let private threeKeywordsData str count = keywordData' threeKeywordsRegex str count 3.
+
+    /// Analyzes the keyword structure of a Web page and returns
+    /// a list of one, two and three keywords combinations data.
+    let analyzeKeywords html language =
+        let stopWords = loadStopWords language
+        let html' = cleanHtml html |> removeStopWords stopWords
+        let matchCollection = regexMatches oneKeywordRegex html'
+        let count = matchCount matchCollection
+        let oneKeyword = keywordData matchCollection html' count 1.
+        let twoKeywords = twoKeywordsData html' count
+        let threeKeywords = threeKeywordsData html' count
+        [oneKeyword; twoKeywords; threeKeywords]
